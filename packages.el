e07c44b7173b3f25caedeb57c449aad6dce217ca
Awab <awab.qureshi@gmail.com>
2022-03-31 17:08:14 +0500

    Added svelte package

--- packages.el
+++ packages.el
@@ -18,6 +18,7 @@
 (package! aas)
 (package! laas)
 (package! counsel-spotify)
+(package! svelte-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
